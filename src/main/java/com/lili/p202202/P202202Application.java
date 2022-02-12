6d5abb7ff8cbc3a7f9706fd48314d78dcec22661
youyou-zero <zt_zero@126.com>
2022-02-12 19:10:24 +0800

    structure

---
+++ src/main/java/com/lili/p202202/P202202Application.java
@@ -0,0 +1,13 @@
+package com.lili.p202202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P202202Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P202202Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lili/p202202/P202202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lili.p202202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P202202ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
